850aded260a9e0e4839d28ea2d456de6444a83cd
Koti Chinmayee <chinmayeekoti8844@gmail.com>
2025-09-04 15:30:13 +0530

    Delete src directory

--- src/routes/propertyRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getProperties, addProperty } = require("../controllers/propertyController");
-
-router.get("/", getProperties);
-router.post("/", addProperty);
-
-module.exports = router;
--- src/routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { registerUser, loginUser } = require("../controllers/userController");
-
-router.post("/register", registerUser); // for signup
-router.post("/login", loginUser);       // for login
-
-module.exports = router;
